99a8e1bd0d5d8e658295b39b68116e3b07c79709
OlushesiToheeb <olaolushesi98@gmail.com>
2020-07-06 17:37:10 +0100

    ft Http calls using fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,6 +15,13 @@ app.use(require('webpack-dev-middleware')( compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id":1, "firstName":"Bob", "lastName":"Smith", "email": "ot@gmail.com"},
+    {"id":2, "firstName":"alice", "lastName":"joe", "email": "wazo@gmail.com"}
+  ])
+})
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'))
 });
